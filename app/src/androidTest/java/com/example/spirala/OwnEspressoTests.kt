88a5fa4fee76db83e860da9932b1031112b45587
Kenan Dizdarević <kdizdarevi1@etf.unsa.ba>
2023-04-27 12:23:58 +0200

    almost finished

--- app/src/androidTest/java/com/example/spirala/OwnEspressoTests.kt
+++ app/src/androidTest/java/com/example/spirala/OwnEspressoTests.kt
@@ -1,12 +1,9 @@
 package com.example.spirala
 
+import android.content.pm.ActivityInfo
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.NoMatchingViewException
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.PositionAssertions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -14,10 +11,9 @@ import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.spirala.GameData.Companion.getAll
-import org.hamcrest.CoreMatchers.allOf
-import org.hamcrest.CoreMatchers.instanceOf
-import org.hamcrest.CoreMatchers.`is` as Is
-import org.junit.Assert.assertTrue
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,8 +21,24 @@ import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class OwnEspressoTests {
-
-
+    @get:Rule
+    var homeRule:ActivityScenarioRule<HomeActivity> = ActivityScenarioRule(HomeActivity::class.java)
+    private fun withIndex(matcher: Matcher<View?>, index: Int): Any {
+        return object : TypeSafeMatcher<View>() {
+            var currentIndex = 0
+            var viewObjHash = 0
+            override fun describeTo(description: Description) {
+                description.appendText(String.format("with index: %d ", index))
+                matcher.describeTo(description)
+            }
+            override fun matchesSafely(view: View): Boolean {
+                if (matcher.matches(view) && currentIndex++ == index) {
+                    viewObjHash = view.hashCode()
+                }
+                return view.hashCode() == viewObjHash
+            }
+        }
+    }
     /**
      * Scenario 1
      * Ovaj scenario testira otvaranje aplikacije u portrait mode-u,
@@ -35,9 +47,6 @@ class OwnEspressoTests {
      */
     @Test
     fun testScenario1(){
-        getAll()
-        //otvaranje aktivnosti
-        var homeRule: ActivityScenario<HomeActivity> = ActivityScenario.launch(HomeActivity::class.java)
         //click na drugu igricu po redu u recycler view-u tj. NBA 2K23
         onView(withId(R.id.game_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
         //provjera da li je su detalji o igri pravilno rasporedjeni na ekranu
@@ -61,5 +70,44 @@ class OwnEspressoTests {
         onView(withId(R.id.homeItem)).perform(click())
         //provjera da li je fragment promijenjen
         onView(withId(R.id.search_button)).check(matches(isCompletelyDisplayed()))
+    }
+    /**
+     * Scenario 2
+     * Ovaj scenario testira da li aplikacija ispravno pamti koja je zadnja igra otvorena u portrait orijentaciji,
+     * tj. da li otvara posljednju otvorenu igricu klikom na details menu item
+     */
+    @Test
+    fun testScenario2(){
+
+        onView(withId(R.id.gameDetailsItem)).check(matches(isNotEnabled()))
+        //onView(withId(R.id.bottom_nav)).perform(NavigationViewActions.navigateTo(R.id.gameDetailsItem)).check(matches(isDisplayed()))
+        onView(withId(R.id.game_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.item_title_textview)).check(matches(withText(getAll()[0].title)))
+        onView(withId(R.id.homeItem)).perform(click())
+        onView(withId(R.id.gameDetailsItem)).check(matches(isEnabled()))
+        onView(withId(R.id.gameDetailsItem)).perform(click())
+        onView(withId(R.id.item_title_textview)).check(matches(withText(getAll()[0].title)))
+    }
+    /**
+     * Scenario 3
+     * Ovaj scenario otvara aplikaciju u landscape orijentaciji
+     * Testira da li je prva otvorena igra prva iz liste igri tj. Fifa 23
+     * Zatim sa vise klikova na elemente recycler view liste testira da li se odgovarajuce igrice otvaraju ispravno
+     * Zatim se vraca na portrait i testira da li su neke funkcionalnosti ok
+     */
+    @Test
+    fun testScenario3(){
+        //promjena orijentacije aplikacije u LANDSCAPE
+        homeRule.scenario.onActivity{ activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+        val games : List<Game> = getAll()
+        //provjera da li se otvori igrica Fifa 23
+        onView(withIndex(withId(R.id.item_title_textview),0) as Matcher<View>?).check(matches(withText(games[0].title)))
+        //klik na zadnji element recycler view-a
+        onView(withId(R.id.game_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
+        //provjera da li je otvorena igrica koja se nalazi na zadnjem mjestu recycler view-a
+        onView(withIndex(withId(R.id.item_title_textview),0) as Matcher<View>?).check(matches(withText(games[1].title)))
+
     }
 }
